59ff50414d249a3cb47b80314c50dfdf83dcfbee
Kershia1 <insatiable.meerkat@gmail.com>
2023-10-21 16:10:47 -0700

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,13 @@
 // const eqArraysTest = require('./test/eqArraysTest');
-
+const assertEqual = require('./assertEqual');
 const eqArrays = (a, b) => /* set 2 vars, resuable to loop any arrs :) */
   a.length === b.length && a.every((element, index) => element === b[index]);
 console.log(eqArrays);
 
-// const arrV = [1,2,3,4]; 
-// const arrX = [1,2,3,4];
+//Sample Test
 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
 // eqArraysTest(eqArrays(arrV, arrX), true);
 
