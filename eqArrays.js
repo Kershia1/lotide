19126dc3e774e392aa459881e484ef229baf8e86
Kershia1 <insatiable.meerkat@gmail.com>
2023-10-21 16:21:18 -0700

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -5,9 +5,11 @@ const eqArrays = (a, b) => /* set 2 vars, resuable to loop any arrs :) */
 console.log(eqArrays);
 
 //Sample Test
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2]), true); // => should FAIL arr.length !== @index [2]
+assertEqual(eqArrays([1, 2, 3], [1, , 3]), true); // => should FAIL undefined @ index [1]
+assertEqual(eqArrays([1, 2, 3], [1, 2, "k"]), true); // => should FAIL string "k" vs integer @ index[2]
+assertEqual(eqArrays(["1", 2, 3], [1, 2, 3]), true); // => should FAIL string "1" vs integer @ index[2] testing for implied corecion
 
 // eqArraysTest(eqArrays(arrV, arrX), true);
 
