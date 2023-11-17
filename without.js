e4e96699ce064b0cbf6c903209b031ab9e975f1a
Kershia1 <insatiable.meerkat@gmail.com>
2023-11-17 19:18:47 +0000

    re-wrote entire without function, over engineered to given purpose and causing multiple bugs. Tested and passing.

--- without.js
+++ without.js
@@ -1,18 +1,32 @@
 const assertArraysEqual = require('./assertArraysEqual');
 
-function without(og, toRemove) {
-  const newArray = [];// new array to push correct elements to
-  for (let h = 0; h < og.length; h++) { //correct 
-    for(let i =0; i < toRemove.length; i++)
-      if( og === toRemove) {
-        newArray.push(og[i])
-        console.log(newArray);
-      }
-  } 
-};
+//Returns a subset of a given array, removing unwanted elements.
 
+function without(array, toRemove) {
+  const newArray = [];
+  for (let i = 0; i <array.length; i++) {
+    if (!toRemove.includes(array[i])) {
+      newArray.push(array[i])
+    }
+  }
+  return newArray;
+}
+
+//Test Cases:
+console.log("Test Case 1: ", without([1, 2, 3], [1]));
+//Expected output: [2, 3]
+
+console.log("Test Case 2: ", without(["1", "2", "3"], [1, 2, "3"]));
+// Expected output:["1", "2"]
+
+console.log('----');
+
+//Assertion Tests:
 
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+//Expected output: These arrays are absolutely equal! 🥳
+
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+//Expected output: TThese arrays are absolutely equal! 🥳
 
 module.exports = without;
