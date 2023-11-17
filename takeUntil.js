6beff31b1053d98022896d61d8783bc922054a4f
Kershia1 <insatiable.meerkat@gmail.com>
2023-11-17 18:38:46 +0000

    Added addtional testing for a differnt callback, arrays of words and numbers. Also add in a line break and a log to return the output to better read the assertions tests in the console, with logged names for the tests, for a better user experience.

--- takeUntil.js
+++ takeUntil.js
@@ -18,28 +18,48 @@ const takeUntil = function (array,callback) {
 
 //Test Cases:
 
-const data = ["I've", "never", "been", "to", "California", ";but,", "I've been", "to", "Washington", "State", "many", "times"];
-const results = takeUntil(data, x => x < 0);
-console.log(results);
+//Case 1 Array of Strings:
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+const data = ["I've", "never", "been", "to", "California", ";but,", "I've been", "to", "Washington", "State", "many", "times"];
+const results = takeUntil(data, x => x === ";but,");
+//Test Case 1:  [ "I've", 'never', 'been', 'to', 'California' ]
+const desiredResults = ["I've", "never", "been", "to", "California"];
+assertArraysEqual(results, desiredResults);
+/* Expected output: These arrays are absolutely equal! 🥳
+[ "I've", 'never', 'been', 'to', 'California' ] [ "I've", 'never', 'been', 'to', 'California', ';but,' ]
+*/
+console.log('Test Case 1: ',results);
 
 console.log('---');
 
+//Case 2 Array of Numbers:
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+//Test Case 1:  [ 1, 2, 5, 7, 2 ]
+const desiredResults1 = [1, 2, 5, 7, 2];
+assertArraysEqual(results1, desiredResults1);
+//Expected output: These arrays are absolutely equal! 🥳
+
+console.log('Test Case 1: ',results1);
+
+console.log('---');
+
+//Case 3 A diffrent Array of Strings:
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const desiredResults2 = ["I've", "been", "to", "Hollywood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+// Test Case 2:  [ "I've", 'been', 'to', 'Hollywood' ]
+assertArraysEqual(results2, desiredResults2);
+//Expected output: These arrays are absolutely equal! 🥳
+console.log('Test Case 2: ',results2);
+console.log('---');
 
+//Case 4 Using a diffrent callback:
 const data3 = ["I've", "never", "been", "to", "California", ";but,", "I've been", "to", "Washington", "State", "many", "times"];
+const desiredResults3 = ["I've", "never", "been", "to", "California"];
+//Test Case 3:  undefined
 const results3 = takeUntil(data3, x => x < 0);
-console.log(results3);
-
-assertArraysEqual(results1, results2); //fail 
-//Expected output: 
-
-assertArraysEqual(results, results3); //pass
-//Expected output: 
-
+assertArraysEqual(results3, desiredResults3);
+//Expected output: These arrays are not an absolute match! ❌
+console.log('Test Case 3: ',results3);
 module.exports = takeUntil;
