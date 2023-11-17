79cc14a78fba575603a7c3e1e6fae509350a0cb1
Kershia1 <insatiable.meerkat@gmail.com>
2023-11-17 02:50:51 +0000

    undid recursion, re-read the assingment, used a a simple array spread and pushed to the empty flattened array, removed mutiple unecessary comments.

--- flatten.js
+++ flatten.js
@@ -2,43 +2,36 @@ const assertArraysEqual = require('./assertArraysEqual');
 
 const flatten = () => {
   const flattenedArray = [];
-
-  // recurse(0, nestedArray, flattenedArray); //basic for loop to iterate
-  // return flattenedArray;// output to assess 
-  
-  function recurse(nestedArray) {
-    for (let i =0; i <nestedArray.length; i++) {
-    if(i >= nestedArray.length)
-    return;// iterate along length of nested array
-    if(Array.isArray(nestedArray[i])) { //array.isArray @[i] assess nArr
-      recurse(nestedArray[i]); //recursion is scary
+  for (let i = 0; i < nestedArray.length; i++) {
+    if (Array.isArray(nestedArray[i])) {
+      flattenedArray.push(...nestedArray[i]);
     } else {
-      flattenedArray.push(nestedArray[i]); //push new array to 1 level
-     }
+      flattenedArray.push(nestedArray[i]);
     }
   }
-  recurse(nestedArray); //begin recursion
   return flattenedArray;
 };
 
-const nestedArray = [1,2,3,[4,5,6],7,8,9]; // simple to depth 1
+//Test Cases:
+
+const nestedArray = [1, 2, 3, [4, 5, 6], 7, 8, 9]; // simple to depth 1
 console.log(flatten(nestedArray)); //[1, 2, 3, 4, 5, 6, 7, 8, 9]
 //"These arrays are absolutely equal! 🥳"
 
 //Tests
-const nestedArray1 = [1,5,6,3,4,8,[9,0]];
-const flattenedArray1 = flatten(nestedArray1); 
-assertArraysEqual(flattenedArray1, [1,5,6,3,4,8,9,0]);
+const nestedArray1 = [1, 5, 6, 3, 4, 8, [9, 0]];
+const flattenedArray1 = flatten(nestedArray1);
+assertArraysEqual(flattenedArray1, [1, 5, 6, 3, 4, 8, 9, 0]);
 // => These arrays are not an absolute match! ❌
 
-const nestedArray2 = [3,51,5,6,3,4,[8,[9,0]]];
-const flattenedArray2= flatten(nestedArray2); 
-assertArraysEqual(flattenedArray2, [3,51,5,6,3,4,8,9,0]);
+const nestedArray2 = [3, 51, 5, 6, 3, 4, [8, 9, 0]];
+const flattenedArray2 = flatten(nestedArray2);
+assertArraysEqual(flattenedArray2, [3, 51, 5, 6, 3, 4, 8, 9, 0]);
 // => These arrays are not an absolute match! ❌
 
-const nestedArray3 = [3,51,5,6,3,4,[8,[9,0]]];
-const flattenedArray3= flatten(nestedArray3); 
-assertArraysEqual(flattenedArray3, [[3,51,[5,[6]]],3,4,8,9,0]);
+const nestedArray3 = [3, 51, 5, 6, 3, 4, [8, [9, 0]]];
+const flattenedArray3 = flatten(nestedArray3);
+assertArraysEqual(flattenedArray3, [[3, 51, [5, [6]]], 3, 4, 8, 9, 0]);
 // => These arrays are not an absolute match! ❌
 
-module.exports = flatten; 
+module.exports = flatten;
