b4ece1d8cb73c5e1ec1639e3ad777cfc2cf6d855
Kershia1 <insatiable.meerkat@gmail.com>
2023-11-17 05:03:46 +0000

    removed uneccessary commented old code, refactored.

--- middle.js
+++ middle.js
@@ -2,10 +2,11 @@
 // if less than 3 it shall return an empty array
 
 const middle = (arr) => {
-  let mid = Math.floor((arr.length) / 2);
   if (arr.length < 3) {
     return [];
   } else {
+    let mid = Math.floor((arr.length) / 2);
+
     if (arr.length % 2 === 0 && arr.length >= 4) {
       return arr.slice(mid - 1, mid + 1);
     }
@@ -14,20 +15,3 @@ const middle = (arr) => {
 };
 
 module.exports = middle;
-
-/*
-const middle = (arr) => {
-  let mid = Math.floor((arr.length) / 2);
-  if (arr.length % 2 === 0 && arr.length > 3) {
-    return [arr[mid - 1], arr[mid]];// 2 middle less 1 from middle and mid value s
-  } else if  (arr.length % 2 === 0 && arr.length < 4) {
-    return arr.slice(mid, mid + 1);
-  } else if (arr.length === 3) {
-  //return arr only at index[1] middle of 3 element array
-    return [arr[mid]];
-  } else {
-    (arr.length < 3);
-    return [];
-  }
-};
-*/
