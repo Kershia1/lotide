562abfc281a53d113d2b6e6ca9a5e253fcb6f832
Kershia1 <insatiable.meerkat@gmail.com>
2023-11-17 07:12:06 +0000

    reviewed how test cases need to be implemented.

--- tail.js
+++ tail.js
@@ -30,22 +30,28 @@ assertEqual(words.length, 3);
 //Check 1st element of the returned array matches the 2ed element of the original 
 const words1 = ["Yo Yo", "Lighthouse", "Labs"];
 const words1Result = tail(words1);
+//Expected output: Assertion Passed: 3 === 3 ✅
 assertEqual(words1Result.length[0], words1.length[1]);
+//Expected output: Assertion Passed: undefined === undefined ✅
 
 // Test Case: Check the new Array
 const words2 = ["Hello"];
 const words2Result = tail(["Hello"]); 
-assertEqual((words2Result.length[0], undefined));
-//Expected output: []
+assertEqual(words2Result.length, 0);
+//Expected output: Assertion Passed: 0 === 0 ✅
+ assertEqual(words2Result[0],undefined);
+//Expected output: Assertion Passed: undefined === undefined ✅
 
 const words3 = [];
 const words3Result = tail([]); 
+assertEqual(words3Result.length, 0);
+//Expected output: Assertion Passed: 0 === 0 ✅
 assertEqual((words3Result.length[0], undefined));
-//Expected output: []
-
+//Expected output: Assertion Passed: undefined === undefined ✅
 
 const words4 = ["Yo Yo", "Lighthouse", "Labs"];
 const words4Result = tail(words4);
-assertEqual(words4Result.length[0], words4.length[1]);
+assertEqual(words4Result[0], words4[1]);
+//Expected output: Assertion Passed: Lighthouse === Lighthouse ✅
 
 module.exports = tail;
