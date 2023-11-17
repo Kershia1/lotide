5aaf4c28d716ac82b7381d9dc604cf593157b2c4
Kershia1 <insatiable.meerkat@gmail.com>
2023-11-17 02:00:36 +0000

    de-bugged and rewrote code to meet assingment expectataions.

--- findKey.js
+++ findKey.js
@@ -1,43 +1,53 @@
 const assertEqual = require('./assertEqual');
 
-//define function and create callback
-function findKey(object, callback) {
-  for ( key in object) { // look inside object
-    if(object.hasOwnProperty(key) && callback(object[key])) { //are there keys
-      return key; //if there are return
+const findKey = function(object, callback) {
+  for (let key in object) {
+    //break this down passing too much in 1 conditional statement
+    if (object.hasOwnProperty(key)) {
+      if (typeof callback === 'function' && callback(object[key])) { //still not a function, remeber cats as a service
+        return key;
+      }
     }
-    return callback;
   }
-  return undefined; //if not nothing to return
+  return undefined;
 };
 
-  const result = findKey({
+const result = findKey({
   "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2); // => "noma"
 
-callback = (x) => {
-  return (result, callback)
-};
 
-assertEqual(findKey(result, "noma"), true, "noma"); // "noma"
-  
-const testObject = {
-  a: 1,
-  b: 2,
-  c: 3,
-  d: 4
-}; 
+assertEqual(result, "noma"); //"noma"
 
-function oddity(test) { 
-  return test % 1 === 0;
-}
+/* eslint error 
+ * 
 
-const results = findKey(testObject,oddity); 
-console.log(results); // 1st odd value found is 1 returns a. 
+error  Do not access Object.prototype method 'hasOwnProperty' from target object  no-prototype-builtins
 
-  module.exports = findKey;
+ * https://stackoverflow.com/questions/39282873/object-hasownproperty-yields-the-eslint-no-prototype-builtins-error-how-to
+ * 
+ * 
+ * suggestion 
+ * Object.prototype.hasOwnProperty.call(obj, prop);
+ * 
+less fiddly option
+This is the way to go in 2022. It has good browser support, is shorter and easier to remember than the accepted answer, and solves the problem ESLint is complaining about. MDN says, Note: Object.hasOwn() is intended as a replacement for Object.hasOwnProperty(). See tomdn.com?object.hasOwn. – 
+salsbury
+ Aug 19, 2022 at 15:20 
+
+ Object.hasOwn(obj, prop)
+
+ Parameters
+obj
+The JavaScript object instance to test.
+
+prop
+The String name or Symbol of the property to test.
+ */
+
+module.exports = findKey;
