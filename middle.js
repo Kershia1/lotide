7f68b68dbf5b8a0bc3e0c90eb2474d3e361e5518
Kershia1 <insatiable.meerkat@gmail.com>
2023-11-17 05:12:35 +0000

    added test cases back in and kept.

--- middle.js
+++ middle.js
@@ -2,16 +2,28 @@
 // if less than 3 it shall return an empty array
 
 const middle = (arr) => {
+  let mid = Math.floor((arr.length) / 2);
   if (arr.length <= 2) {
     return [];
   } else {
-    let mid = Math.floor((arr.length) / 2);
-
     if (arr.length % 2 === 0 && arr.length >= 4) {
       return arr.slice(mid - 1, mid + 1);
     }
   }
   return [arr[mid]];
 };
+
+//Test Variable and Cases
+const arr1 = [1,2,3];//2 returns undefined
+const arr2 = [1,2,3,4]; //3,4 returns undefined
+const arr3 = [1,2]; // []
+
+
+console.log(middle(arr1)); 
+//expected output [2]
+console.log(middle(arr2));
+//expected output [2,3]
+console.log(middle(arr3));
+//expected output []
 
 module.exports = middle;
