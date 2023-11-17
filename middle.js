111ddd1e0a6bbb4d58835b2c3c072733bdf22f95
Kershia1 <insatiable.meerkat@gmail.com>
2023-11-17 03:32:23 +0000

    re did access the elements at the correct locatons.

--- middle.js
+++ middle.js
@@ -2,16 +2,16 @@
 // if less than 3 it shall return an empty array
 
 const middle = (arr) => {
-  let mid = Math.floor((arr.length) /2);
-  if (arr.length % 2 === 0 && arr.length >= 4) {
-    return arr.slice(mid, mid + 1);
+  let mid = Math.floor((arr.length) / 2);
+  if (arr.length < 3) {
+    return [];
   } else {
-    if (arr.length < 3) {
-      return [];
+    if (arr.length % 2 === 0 && arr.length >= 4) {
+      return arr.slice(mid - 1, mid + 1);
     }
   }
-  return mid;
-}
+  return [arr[mid]];
+};
 
 module.exports = middle;
 
