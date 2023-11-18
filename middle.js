ca63147321ee3009cb965035a0195e00f6de4a38
Kershia1 <insatiable.meerkat@gmail.com>
2023-11-18 02:00:09 +0000

    Rewrote middle function, tested, linted. All passing.

--- middle.js
+++ middle.js
@@ -3,14 +3,31 @@
 
 const middle = (arr) => {
   let mid = Math.floor((arr.length) / 2);
+  let newArray = [];
+
   if (arr.length <= 2) {
     return [];
-  } else {
-    if (arr.length % 2 === 0) {
-      return arr.slice(mid - 1, mid + 1);
-    }
   }
-  return [arr[mid]];
+
+  if (arr.length % 2 === 0) {
+    newArray.push((mid), (mid + 1));
+  } else {
+    newArray.push(mid + 1);
+  }
+  return newArray;
 };
+
+
+//Test Variable and Cases
+const arr1 = [1, 2, 3];
+const arr2 = [1, 2, 3, 4];
+const arr3 = [1, 2];
+
+console.log(middle(arr1));
+//expected output [2]
+console.log(middle(arr2));
+//expected output [2,3]
+console.log(middle(arr3));
+//expected output []
 
 module.exports = middle;
